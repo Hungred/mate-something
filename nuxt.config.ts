2b058bfa75905f826e7a3440fa0e226867b854ef
Anita <anita397892@gamil.com>
2026-01-09 23:44:26 +0800

    feat: 新增i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/ui'],
+  modules: ['@nuxt/image', '@nuxt/ui', '@nuxtjs/i18n'],
   css: ['~/assets/css/main.css'],
+  i18n: {
+    strategy: 'prefix_except_default',
+    defaultLocale: 'zh-TW',
+    locales: [
+      { code: 'zh-TW', name: '繁體中文', file: 'zh.json' },
+      { code: 'en', name: 'English', file: 'en.json' },
+    ],
+    langDir: 'language/',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
+      alwaysRedirect: true,
+      fallbackLocale: 'zh-TW',
+    },
+  },
 });
