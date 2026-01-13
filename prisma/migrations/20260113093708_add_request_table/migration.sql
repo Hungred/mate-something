071a5878514ac6525ba3f78fd98f647221d0b323
Anita <anita397892@gamil.com>
2026-01-13 19:28:46 +0800

    feat: add request table sql

---
+++ prisma/migrations/20260113093708_add_request_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `requests` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `content` TEXT NULL,
+    `roomNumber` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL DEFAULT 'open',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
