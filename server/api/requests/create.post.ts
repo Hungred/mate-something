6f325a8347d2d08040c4c9f08196dbd161e981e3
Anita <anita397892@gmail.com>
2026-01-21 20:44:19 +0800

    fix: fix typo

--- server/api/requests/create.post.ts
+++ server/api/requests/create.post.ts
@@ -1,11 +1,10 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
-
   const newRequest = await prisma.requests.create({
     data: {
       title: body.title,
       content: body.content,
-      roomId: body.roomId,
+      room_id: body.roomId,
     },
   });
 
